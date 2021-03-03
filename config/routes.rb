f987a4adb0ee2e64a1aada213b596762d0800f1e
Paschalis Konstantinou <pasc.konstantinou@gmail.com>
2021-03-03 17:36:22 +0100

    Styling dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  get '/dashboard' => "pages#dashboard", :as => :user_dashboard
   root to: 'pages#home'
   get '/dashboard', to: 'pages#dashboard'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
