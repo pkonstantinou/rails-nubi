1a70213d63116d505cfc74fb76b226690e00f5a2
Paschalis Konstantinou <pasc.konstantinou@gmail.com>
2021-03-04 00:28:17 +0100

    Created dashboard routes-controller-views

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,16 @@
+class DashboardController < ApplicationController
+  def my_tracks
+  end
+
+  def track_library
+  end
+
+  def team
+  end
+
+  def profile
+  end
+
+  def settings
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  get '/dashboard' => "pages#dashboard", :as => :user_root
+  # get '/dashboard' => "pages#dashboard", :as => :user_root
   root to: 'pages#home'
   get '/dashboard', to: 'pages#dashboard'
+  get '/dashboard/my-tracks', to: 'dashboard#my_tracks'
+  get '/dashboard/track-library', to: 'dashboard#track_library'
+  get '/dashboard/team', to: 'dashboard#team'
+  get '/dashboard/profile', to: 'dashboard#profile'
+  get '/dashboard/settings', to: 'dashboard#settings'
 
   resources :tracks, except: %i[show] do
     resources :steps, only: %i[create destroy]
