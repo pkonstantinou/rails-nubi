3c8cdd6987f65c57ce91550257bb3146af8a3a20
Paschalis Konstantinou <pasc.konstantinou@gmail.com>
2021-03-05 02:26:17 +0100

    Added back btn

--- app/helpers/sidebar_helper.rb
+++ app/helpers/sidebar_helper.rb
@@ -5,5 +5,6 @@ module SidebarHelper
     return 'team' if params[:controller] == 'dashboard' && params[:action] == 'team'
     return 'profile' if params[:controller] == 'dashboard' && params[:action] == 'profile'
     return 'settings' if params[:controller] == 'dashboard' && params[:action] == 'settings'
+    return 'track-library' if params[:controller] == 'tracks' && params[:action] == 'edit'
   end
 end
