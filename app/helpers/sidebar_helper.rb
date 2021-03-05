a0c9ab7a9e67f8e9c88c22e4b6b7dc244d3c2e0a
Paschalis Konstantinou <pasc.konstantinou@gmail.com>
2021-03-05 01:20:24 +0100

    Further progress

--- app/helpers/sidebar_helper.rb
+++ app/helpers/sidebar_helper.rb
@@ -2,5 +2,8 @@ module SidebarHelper
   def sidebar_active_link(params)
     return 'my-tracks' if params[:controller] == 'dashboard' && params[:action] == 'my_tracks'
     return 'track-library' if params[:controller] == 'dashboard' && params[:action] == 'track_library'
+    return 'team' if params[:controller] == 'dashboard' && params[:action] == 'team'
+    return 'profile' if params[:controller] == 'dashboard' && params[:action] == 'profile'
+    return 'settings' if params[:controller] == 'dashboard' && params[:action] == 'settings'
   end
 end
