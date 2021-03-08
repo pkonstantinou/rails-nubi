016d62477a63808b87888671111c8a4d44dbde31
Tom Louwagie <tom@louwagie.com>
2021-03-08 14:48:26 +0100

    Moved file icon to left of step card

--- app/helpers/media_helper.rb
+++ app/helpers/media_helper.rb
@@ -8,11 +8,11 @@ module MediaHelper
   def media_link_generator(object)
     case object.content_type
     when 'Video'
-      link_to 'Video', cloudinary_url(object.media.key, resource_type: :video), target: :_blank
+      link_to '<i class="fas fa-video fa-lg"></i>'.html_safe, cloudinary_url(object.media.key, resource_type: :video), target: :_blank, class: "link-styling"
     when 'Image'
-      link_to 'Image', cloudinary_url(object.media.key, resource_type: :image), target: :_blank
+      link_to '<i class="far fa-image fa-lg"></i>'.html_safe, cloudinary_url(object.media.key, resource_type: :image), target: :_blank, class: "link-styling"
     when 'PDF'
-      link_to 'PDF', cloudinary_url(object.media.key, resource_type: :image), target: :_blank
+      link_to '<i class="far fa-file-pdf fa-lg"></i>'.html_safe, cloudinary_url(object.media.key, resource_type: :image), target: :_blank, class: "link-styling"
     end
   end
 end
